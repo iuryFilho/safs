835f8d36ec71fd3c68f62d5fec4df32d04fcc8f1
Iury Filho <iuryfmmf@gmail.com>
2025-05-15 17:28:23 -0300

    commit inicial

---
+++ TesteGraficoPython/extraction.py
@@ -0,0 +1,199 @@
+import glob
+import os
+import pandas as pd
+import json
+
+
+def filter_metric(
+    metric: str, simulation_results: list[pd.DataFrame]
+) -> list[pd.DataFrame]:
+    """
+    Extracts the filtered metrics from the simulation results files.
+    Args:
+        metric (str): The metric to filter by.
+        simulation_results (list[DataFrame]): List of DataFrames containing the simulation results.
+    Returns:
+        list: A list of DataFrames containing the filtered metrics.
+    """
+
+    return [d[d.Metrics == metric] for d in simulation_results]
+
+
+def extract_labels_from_paths(paths: list[str]) -> list[str]:
+    """
+    Extracts the labels from the file paths.
+    Args:
+        paths (list[str]): List of file paths.
+    Returns:
+        list: A list of labels extracted from the file paths.
+    """
+
+    solutions = [s.split("/")[-2] for s in paths]
+    return [s.split("_", 1)[0] for s in solutions]
+
+
+def load_simulation_results(paths: list[str]) -> list[pd.DataFrame]:
+    """
+    Loads the simulation results from the given file paths.
+    Args:
+        paths (list[str]): List of file paths to the simulation results.
+    Returns:
+        list: A list of DataFrames containing the simulation results.
+    """
+
+    return [pd.read_csv(s) for s in paths]
+
+
+def get_metric_names(path: str) -> list[str]:
+    """
+    Retrieves the metric names from the given file path.
+    Args:
+        path (str): The file path to the simulation results.
+    Returns:
+        list: A list of metric names found in the file.
+    Raises:
+        FileNotFoundError: If the file does not exist.
+    """
+
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"The file '{path}' does not exist.")
+    with open(path, "r") as file:
+        first_line = file.readline()
+        sep = "," if "," in first_line else ";"
+    df = pd.read_csv(path, sep=sep)
+    return list(df["Metrics"].unique())
+
+
+def get_paths_by_metric(base_directory: str, metric: str) -> list[str]:
+    """
+    Retrieves the csv file paths from the base directory based on the given metric.
+    Args:
+        base_directory (str): The base directory to search for files.
+        metric (str): The file name pattern to search for.
+    Returns:
+        list: A list of csv file paths matching the given pattern.
+    Raises:
+        FileNotFoundError: If the base directory does not exist.
+    """
+
+    if not os.path.exists(base_directory):
+        raise FileNotFoundError(
+            f"The base directory '{base_directory}' does not exist."
+        )
+    pattern = base_directory + "/*/*" + metric
+    file_paths = glob.glob(pattern)
+    return [s.replace("\\", "/") for s in file_paths]
+
+
+def get_csv_by_directory(simulation_directory: str) -> list[str]:
+    """
+    Retrieves the csv file paths from the simulation directory.
+    Args:
+        simulation_directory (str): The simulation directory to search for files.
+    Returns:
+        list: A list of csv file paths found in the directory.
+    Raises:
+        FileNotFoundError: If the base directory does not exist.
+    """
+    if not os.path.exists(simulation_directory):
+        raise FileNotFoundError(
+            f"The simulation directory '{simulation_directory}' does not exist."
+        )
+    pattern = simulation_directory + "/*.csv"
+    file_paths = glob.glob(pattern)
+    return [s.replace("\\", "/") for s in file_paths]
+
+
+def extract_metric_group_names(file_paths: list[str]) -> list[str]:
+    """
+    Extracts the metric group names from the given file paths.
+    Args:
+        simulation_directory (str): The simulation directory to search for files.
+    Returns:
+        list: A list of metric group names found in the file paths.
+    """
+    file_names = [s.replace("\\", "/").split("/")[-1] for s in file_paths]
+    return [s.split("_")[-1].split(".")[0] for s in file_names]
+
+
+def get_simulations_directories(base_directory: str) -> tuple[list[str], str]:
+    """
+    Retrieves the simulation directories from the base directory.
+    Args:
+        base_directory (str): The base directory to search for files.
+    Returns:
+        (list, error): A list of simulation directories found in the directory and an error if the base directory does not exist.
+    """
+    if not os.path.exists(base_directory):
+        return [], f"The base directory '{base_directory}' does not exist."
+    pattern = base_directory + "/*/"
+    paths = glob.glob(pattern)
+    return [s.replace("\\", "/") for s in paths], None
+
+
+def group_metrics(
+    metric_groups: list[str], file_paths: list[str]
+) -> dict[str, list[str]]:
+    """
+    Groups the metrics by their names and file paths.
+    Args:
+        metric_groups (list): A list of metric groups.
+        file_paths (list): A list of file paths.
+    Returns:
+        dict: A dictionary with metric groups as keys and file paths as values.
+    """
+
+    grouped_metrics = {}
+    for metric_group, path in zip(metric_groups, file_paths):
+        metrics = get_metric_names(path)
+        grouped_metrics[metric_group] = metrics
+    return grouped_metrics
+
+
+def load_config(path: str) -> tuple[dict, str]:
+    """
+    Loads the configuration from a JSON file.
+    Args:
+        path (str): The path to the configuration file.
+    Returns:
+        (dict, error): A dictionary with the configuration data and an error if the file does not exist.
+    """
+    if not path:
+        return {}, None
+    try:
+        with open(path, "r") as config_file:
+            return json.load(config_file), None
+    except FileNotFoundError:
+        return {}, f"The configuration file '{path}' does not exist."
+    except json.JSONDecodeError:
+        return {}, f"The configuration file '{path}' is not a valid JSON file."
+
+
+def save_config(config: dict, path: str) -> tuple[str, str]:
+    """
+    Saves the configuration to a JSON file.
+    Args:
+        config (dict): The configuration data to save.
+        path (str): The path to save the configuration file.
+    Returns:
+        (str, error): A message indicating the result of the save operation and an error if the file could not be saved.
+    """
+    if not path:
+        return "No path provided", None
+    try:
+        with open(path, "w") as config_file:
+            json.dump(config, config_file, indent=4)
+        return "Configuration saved successfully", None
+    except IOError:
+        return "Failed to save configuration", f"Could not write to '{path}'"
+
+
+if __name__ == "__main__":
+    # Example usage
+    base_directory = "TesteGraficoPython/simulations"
+    simulation_directories, err = get_simulations_directories(base_directory)
+    file_paths = get_csv_by_directory(simulation_directories[0])
+    metric_groups = extract_metric_group_names(file_paths)
+    grouped_metrics = group_metrics(metric_groups, file_paths)
+    with open("result.json", "w") as result_file:
+        result_file.write(json.dumps(grouped_metrics, indent=4))
---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request
+from markupsafe import escape
+import TesteGraficoPython.extraction as ex
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.jinja")
+
+
+@app.route("/choose-metrics", methods=["GET", "POST"])
+def choose_metrics():
+    """
+    Handles the selection of metrics and displays the corresponding CSV files.
+    """
+    if request.method == "POST":
+        base_directory = request.form["base-directory"]
+        simulation_directories, err = ex.get_simulations_directories(base_directory)
+        if err:
+            return render_template(
+                "choose_metrics.jinja",
+                base_dir_error=err,
+                base_directory=base_directory,
+                config_file=config_file,
+            )
+
+        csv_paths = ex.get_csv_by_directory(simulation_directories[0])
+        metric_groups = ex.extract_metric_group_names(csv_paths)
+        grouped_metrics = ex.group_metrics(metric_groups, csv_paths)
+
+        config_file = request.form["config-file"]
+        config_data, err = ex.load_config(config_file)
+        if err:
+            return render_template(
+                "choose_metrics.jinja",
+                config_file_error=err,
+                base_directory=base_directory,
+                config_file=config_file,
+                grouped_metrics=grouped_metrics,
+            )
+
+        output_config_file = request.form.get("output-config-file", "")
+        if output_config_file:
+            new_config_data = {}
+            for metric_group, metric_list in config_data.items():
+                for metric in metric_list:
+                    if metric in request.form:
+                        new_config_data[metric_group] = new_config_data.get(
+                            metric_group, []
+                        ) + [metric]
+            ex.save_config(new_config_data, output_config_file)
+
+        return render_template(
+            "choose_metrics.jinja",
+            base_directory=base_directory,
+            config_file=config_file,
+            simulation_directories=simulation_directories,
+            grouped_metrics=grouped_metrics,
+            config_data=config_data,
+        )
+    else:
+        return render_template("choose_metrics.jinja")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
