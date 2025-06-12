52a9d7087a6d02bdd8da3ea62fc60cbeda6a87c6
Iury Filho <iuryfmmf@gmail.com>
2025-06-12 01:08:18 -0300

    refactor: Simplifica geração de gráficos

--- src/services/graphs_service.py
+++ src/services/graphs_service.py
@@ -38,6 +38,25 @@ def generate_graphs(
     metric_type: str = "individual",
     graph_type: str = "line",
 ):
+    """
+    Generate graphs based on the provided parameters.\\
+    This function compiles data for individual or grouped metrics and generates
+    graphs based on the specified graph type (line, bar, or stacked-bar).
+    Args:
+        base_directory: Base directory where the simulation results are stored.
+        directories: List of directories containing simulation results.
+        grouped_metrics: Dictionary containing grouped metrics.
+        labels: List of labels for the graphs (optional).
+        loads: List of loads for the x-axis (optional).
+        load_points: List of load points for the x-axis (optional).
+        fontsize: Font size for the labels (default is "large").
+        figsize: Size of the figure for the graph (default is (10, 5)).
+        overwrite: Whether to overwrite existing files (default is True).
+        metric_type: Type of metrics to generate graphs for
+            ("individual" or "grouped", default is "individual").
+        graph_type: Type of graph to generate
+            ("line", "bar", or "stacked-bar", default is "line").
+    """
     if metric_type == "individual":
         generate_graphs_func = generate_individual_graphs
     elif metric_type == "grouped":
@@ -51,9 +70,10 @@ def generate_graphs(
     full_directories = [op.join(base_directory, d) for d in directories]
     for metric_group, metrics in grouped_metrics.items():
         simulation_results = ss.load_simulation_results(full_directories, metric_group)
+        metric_group_alias = METRIC_GROUP_ALIASES[metric_group]
         generate_graphs_func(
             metrics=metrics,
-            metric_group=metric_group,
+            metric_group=metric_group_alias,
             simulation_results=simulation_results,
             labels=labels,
             loads=loads,
@@ -68,19 +88,34 @@ def generate_graphs(
 
 
 def generate_individual_graphs(
-    metrics: list[str] = [],
-    simulation_results: list[pd.DataFrame] = [],
-    labels: list[str] = [],
-    loads: list[str] = [],
-    load_points: list[str] = [],
-    graph_type: str = "line",
+    metrics: list[str],
+    simulation_results: list[pd.DataFrame],
+    labels: list[str],
+    loads: list[str],
+    load_points: list[str],
     filename_prefix: str = "",
-    x_label: str = "",
-    fontsize: str = "large",
-    figsize: tuple[int, int] = (10, 5),
-    overwrite: bool = True,
-    **_,
+    **graph_kwargs: dict[str, any],
 ):
+    """
+    Generate graphs for individual metrics.\\
+    This function compiles data for each individual metric and generates graphs
+    based on the provided parameters.
+    Args:
+        metrics: List of individual metrics to plot.
+        simulation_results: List of DataFrames containing simulation results.
+        labels: List of labels for the graphs.
+        loads: List of loads for the x-axis.
+        load_points: List of load points for the x-axis.
+        filename_prefix: Prefix for the output file names.
+        graph_kwargs: Keyword arguments containing the following keys
+            - **graph_type**: Type of graph to generate
+                (e.g., "line", "bar", "stacked-bar").
+            - **x_label**: Label for the x-axis.
+            - **fontsize**: Font size for the labels.
+            - **figsize**: Size of the figure for the graph.
+            - **overwrite**: Whether to overwrite existing files.
+    """
+    graph_kwargs.pop("metric_group", None)
     for metric in metrics:
         dataframes = ss.compile_data(
             simulation_results,
@@ -88,35 +123,47 @@ def generate_individual_graphs(
             "individual",
             load_points,
         )
-        output_file = f"{filename_prefix}_{metric.replace(' ', '_')}"
+        filename = f"{filename_prefix}_{metric.replace(' ', '_')}"
         plot_graph(
-            graph_type=graph_type,
             dataframes=dataframes,
             loads=loads,
             labels=labels,
-            x_label=x_label,
             y_label=metric,
-            fontsize=fontsize,
-            figsize=figsize,
-            output_file=output_file,
-            overwrite=overwrite,
+            output_file=filename,
+            **graph_kwargs,
         )
 
 
 def generate_grouped_graphs(
-    metrics: list[str] = [],
-    metric_group: str = "",
-    simulation_results: list[pd.DataFrame] = [],
-    labels: list[str] = [],
-    loads: list[str] = [],
-    load_points: list[str] = [],
-    graph_type: str = "line",
+    metrics: list[str],
+    metric_group: str,
+    simulation_results: list[pd.DataFrame],
+    labels: list[str],
+    loads: list[str],
+    load_points: list[str],
     filename_prefix: str = "",
-    x_label: str = "",
-    fontsize: str = "large",
-    figsize: tuple[int, int] = (10, 5),
-    overwrite: bool = True,
+    **graph_kwargs: dict[str, any],
 ):
+    """
+    Generate graphs for grouped metrics.\\
+    This function compiles data for each metric group and generates graphs
+    based on the provided parameters.
+    Args:
+        metrics: List of individual metrics to plot.
+        metric_group: The metric group to which the metrics belong.
+        simulation_results: List of DataFrames containing simulation results.
+        labels: List of labels for the graphs.
+        loads: List of loads for the x-axis.
+        load_points: List of load points for the x-axis.
+        filename_prefix: Prefix for the output file names.
+        graph_kwargs: Keyword arguments containing the following keys
+            - **graph_type**: Type of graph to generate
+                (e.g., "line", "bar", "stacked-bar").
+            - **x_label**: Label for the x-axis.
+            - **fontsize**: Font size for the labels.
+            - **figsize**: Size of the figure for the graph.
+            - **overwrite**: Whether to overwrite existing files.
+    """
     for label, simulation_result in zip(labels, simulation_results):
         dataframes = ss.compile_data(
             simulation_result,
@@ -124,22 +171,19 @@ def generate_grouped_graphs(
             "grouped",
             load_points,
         )
-        output_file = f"{filename_prefix}_{METRIC_GROUP_ALIASES[metric_group]}_{label}"
+        filename = f"{filename_prefix}_{metric_group}_{label}"
         plot_graph(
-            graph_type=graph_type,
             dataframes=dataframes,
             loads=loads,
             labels=ms.get_metrics_components(metrics),
-            x_label=x_label,
             y_label=ms.get_metric_root(metrics[0]),
-            fontsize=fontsize,
-            figsize=figsize,
-            output_file=output_file,
-            overwrite=overwrite,
+            output_file=filename,
+            **graph_kwargs,
         )
 
 
 def plot_graph(
+    *,
     graph_type: str,
     dataframes: list[pd.DataFrame],
     loads: list[str],
@@ -155,6 +199,26 @@ def plot_graph(
     max_columns: int = 5,
     frameon: bool = True,
 ):
+    """
+    Plot a graph based on the provided parameters.\\
+    This function generates a graph using the specified type (line, bar, or stacked-bar)
+    and saves it to the specified output file.
+    Args:
+        graph_type: Type of graph to generate (e.g., "line", "bar", "stacked-bar").
+        dataframes: List of DataFrames containing the data to plot.
+        loads: List of loads for the x-axis.
+        labels: List of labels for the graph legend.
+        x_label: Label for the x-axis.
+        y_label: Label for the y-axis.
+        fontsize: Font size for the labels.
+        figsize: Size of the figure for the graph.
+        output_file: Path to save the output file (without extension).
+        overwrite: Whether to overwrite existing files.
+        legend_position: Position of the legend in the graph.
+        bbox_to_anchor: Bounding box to anchor the legend.
+        max_columns: Maximum number of columns in the legend.
+        frameon: Whether to draw a frame around the legend.
+    """
     plt.figure(figsize=figsize)
 
     if graph_type == "line":
@@ -185,7 +249,20 @@ def plot_graph(
     plt.close()
 
 
-def plot_line_graph(dataframes, loads, labels):
+def plot_line_graph(
+    dataframes: list[pd.DataFrame],
+    loads: list[str],
+    labels: list[str],
+):
+    """
+    Plot a line graph with error bars.\\
+    This function generates a line graph for the provided dataframes,
+    loads, and labels, with error bars representing the uncertainty in the data.
+    Args:
+        dataframes (list[pd.DataFrame]): List of DataFrames containing the data to plot.
+        loads (list[str]): List of loads for the x-axis.
+        labels (list[str]): List of labels for the graph legend.
+    """
     load_positions = list(range(len(loads)))
     for i in range(len(dataframes)):
         y = dataframes[i]["mean"]
@@ -202,7 +279,20 @@ def plot_line_graph(dataframes, loads, labels):
     plt.xticks(load_positions, loads)
 
 
-def plot_bar_graph(dataframes, loads, labels):
+def plot_bar_graph(
+    dataframes: list[pd.DataFrame],
+    loads: list[str],
+    labels: list[str],
+):
+    """
+    Plot a bar graph with error bars.\\
+    This function generates a bar graph for the provided dataframes,
+    loads, and labels, with error bars representing the uncertainty in the data.
+    Args:
+        dataframes (list[pd.DataFrame]): List of DataFrames containing the data to plot.
+        loads (list[str]): List of loads for the x-axis.
+        labels (list[str]): List of labels for the graph legend.
+    """
     bar_width = 0.15
     load_positions = list(range(len(loads)))
     for i in range(len(dataframes)):
@@ -223,7 +313,20 @@ def plot_bar_graph(dataframes, loads, labels):
         )
 
 
-def plot_stacked_bar_graph(dataframes, loads, labels):
+def plot_stacked_bar_graph(
+    dataframes: list[pd.DataFrame],
+    loads: list[str],
+    labels: list[str],
+):
+    """
+    Plot a stacked bar graph with error bars.\\
+    This function generates a stacked bar graph for the provided dataframes,
+    loads, and labels, with error bars representing the uncertainty in the data.
+    Args:
+        dataframes (list[pd.DataFrame]): List of DataFrames containing the data to plot.
+        loads (list[str]): List of loads for the x-axis.
+        labels (list[str]): List of labels for the graph legend.
+    """
     bar_width = 0.15
     load_positions = list(range(len(loads)))
     bottom = [0] * len(loads)
